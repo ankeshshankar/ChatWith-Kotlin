e22c86f4cd08eafe96a3a4175074216ff4f7480d
ankeshshankar <kumar.ankeshshiv@gmail.com>
2021-01-28 15:15:54 +0530

    SplashScreen Done

---
+++ app/src/main/java/com/chatwith/app/ui/splash_screen/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.chatwith.app.ui.splash_screen
+
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.chatwith.app.MainActivity
+import com.chatwith.app.R
+import com.chatwith.app.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private var timeOut = 1300
+    @SuppressLint("NewApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.statusBarColor = this.resources.getColor(R.color.colorPrimary, null)
+        val alpha = PropertyValuesHolder.ofFloat("Alpha", 0f, 1f)
+        val translateY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, -200f, 0f)
+        ObjectAnimator.ofPropertyValuesHolder(binding.imgLogo, alpha, translateY).apply {
+            duration = 800
+            start()
+        }
+        ObjectAnimator.ofPropertyValuesHolder(binding.text, alpha).apply {
+            duration = 1000
+            start()
+
+        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, timeOut.toLong())
+    }
+}
