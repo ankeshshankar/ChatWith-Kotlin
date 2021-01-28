fa893f584545c8ad85169236b87584c1979d1780
ankeshshankar <kumar.ankeshshiv@gmail.com>
2021-01-29 04:10:59 +0530

    Ui Created for chat and and new Contract list

---
+++ app/src/main/java/com/chatwith/app/ui/chat/Contacts.kt
@@ -0,0 +1,12 @@
+package com.chatwith.app.ui.chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chatwith.app.R
+
+class Contacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
