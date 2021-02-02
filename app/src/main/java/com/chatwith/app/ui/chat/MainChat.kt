77dac218fb9145f7f4c1b7dfe15ba0d367d7ee5a
ankeshshankar <kumar.ankeshshiv@gmail.com>
2021-02-02 14:17:50 +0530

    Code fixes

--- app/src/main/java/com/chatwith/app/ui/chat/MainChat.kt
+++ app/src/main/java/com/chatwith/app/ui/chat/MainChat.kt
@@ -13,7 +13,6 @@ import com.chatwith.app.databinding.ActivityMainChatBinding
 import com.chatwith.app.model.Message
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import org.greenrobot.eventbus.EventBus
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.HashMap
@@ -23,7 +22,8 @@ class MainChat : AppCompatActivity() {
     private lateinit var binding: ActivityMainChatBinding
     private val messageAdapter = MessageAdapter()
     private val messageList = arrayListOf<Message>()
-    private lateinit var auth :FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,11 +52,11 @@ class MainChat : AppCompatActivity() {
 
         binding.newChatList.apply {
             this.layoutManager =
-                LinearLayoutManager(
-                        this.context,
-                        LinearLayoutManager.VERTICAL,
-                        false
-                )
+                    LinearLayoutManager(
+                            this.context,
+                            LinearLayoutManager.VERTICAL,
+                            false
+                    )
             this.itemAnimator = DefaultItemAnimator()
             this.adapter = messageAdapter
 
@@ -69,135 +69,50 @@ class MainChat : AppCompatActivity() {
                     readMessage(
                             auth.currentUser?.uid.toString(),
                             intent.getStringExtra("receiverId").toString(),
-                            "second")
+                    )
                 }
             }
         })
 
-
-
-
-
-
-
-
-
-
-
-      /*      var scroll = false
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                when (newState) {
-                    AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL -> {
-                        scroll = true
-                       *//* readMessage(
-                                auth.currentUser?.uid.toString(),
-                                intent.getStringExtra("receiverId").toString(),
-                                "second"
-                        )*//*
-
-                    }
-                }
-            }
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-
-
-                if (recyclerView.scrollState == RecyclerView.SCROLL_STATE_SETTLING)
-                    if(scroll){
-                        readMessage(
-                                auth.currentUser?.uid.toString(),
-                                intent.getStringExtra("receiverId").toString(),
-                                "second"
-                        )
-                    }
-                scroll = false
-
-                super.onScrolled(recyclerView, dx, dy)
-            }*/
         readMessage(
                 auth.currentUser?.uid.toString(),
                 intent.getStringExtra("receiverId").toString(),
-                "first"
         )
 
     }
 
 
+    private fun readMessage(sender: String, receiver: String) {
 
-    private fun readMessage(sender: String, receiver: String, time: String) {
-        //val reference: DatabaseReference
-       /* val rootNode: FirebaseDatabase = FirebaseDatabase.getInstance()
-        val auth = FirebaseAuth.getInstance()
-        reference = rootNode.getReference("Chat").child("messages")*/
-
-
-
-
-        when(time){
-            "first" -> {
-                val reference = FirebaseDatabase.getInstance().reference
-                        .child("Chat").child("messages")
-                        .limitToLast(10)
-                        .orderByChild("timestamp")
-                reference.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        messageList.clear()
-                        for (data in snapshot.children) {
-                            val message = Message(
-                                    sender = data.child("sender").value.toString(),
-                                    receiver = data.child("receiver").value.toString(),
-                                    message = data.child("message").value.toString(),
-                                    timestamp = data.child("timestamp").value.toString(),
-                            )
-                            if (message.sender == sender && message.receiver == receiver || message.sender == receiver && message.receiver == sender) {
-                                messageList.add(message)
-                            }
-                        }
-                        messageAdapter.apply {
-                            setData(messageList)
-                            notifyDataSetChanged()
-                        }
+        val reference = FirebaseDatabase.getInstance().reference
+                .child("Chat").child("messages")
+                .limitToLast(10)
+                .orderByChild("timestamp")
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                messageList.clear()
+                for (data in snapshot.children) {
+                    val message = Message(
+                            sender = data.child("sender").value.toString(),
+                            receiver = data.child("receiver").value.toString(),
+                            message = data.child("message").value.toString(),
+                            timestamp = data.child("timestamp").value.toString(),
+                    )
+                    if (message.sender == sender && message.receiver == receiver || message.sender == receiver && message.receiver == sender) {
+                        messageList.add(message)
                     }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        TODO("Not yet implemented")
-                    }
-
-                })
+                }
+                messageAdapter.apply {
+                    setData(messageList)
+                    notifyDataSetChanged()
+                }
             }
-            "second" -> {
-                val reference = FirebaseDatabase.getInstance().reference
-                        .child("Chat").child("messages")
-                        .limitToLast(10)
-                        .orderByChild("timestamp")
 
-
-                reference.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        for (data in snapshot.children) {
-                            val message = Message(
-                                    sender = data.child("sender").value.toString(),
-                                    receiver = data.child("receiver").value.toString(),
-                                    message = data.child("message").value.toString(),
-                                    timestamp = data.child("timestamp").value.toString(),
-                            )
-                            if (message.sender == sender && message.receiver == receiver || message.sender == receiver && message.receiver == sender) {
-                                messageList.add(message)
-                            }
-                        }
-                        messageAdapter.apply {
-                            setData(messageList)
-                            notifyDataSetChanged()
-                        }
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        TODO("Not yet implemented")
-                    }
-
-                })
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
             }
-        }
+
+        })
 
     }
 
